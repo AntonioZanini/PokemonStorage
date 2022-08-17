d45f80a3456070de526d47eb4e511115883d64cf
AntonioZanini <antoniozfarias@hotmail.com>
2022-08-16 22:41:47 -0300

    Feat: Added to context query functions

---
+++ PokemonStorage.API/Data/EntityQuery.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+
+namespace PokemonStorage.API.Data;
+
+public class EntityQuery<TEntity> where TEntity : class
+{
+    public ICollection<Expression<Func<TEntity, object>>> Includes { get; private set; }
+	public ICollection<Expression<Func<TEntity, bool>>> Filters { get; private set; }
+	public ICollection<SortField<TEntity>> Sorts { get; private set; }
+
+	public EntityQuery()
+	{
+		Includes = new List<Expression<Func<TEntity, object>>>();
+		Filters = new List<Expression<Func<TEntity, bool>>>();
+		Sorts = new List<SortField<TEntity>>();
+	}
+
+	public EntityQuery<TEntity> Filter(Expression<Func<TEntity, bool>> filter)
+	{
+        Filters.Add(filter);
+		return this;
+    }
+
+    public EntityQuery<TEntity> Load(Expression<Func<TEntity, object>> include)
+	{
+		Includes.Add(include);
+		return this;
+	}
+
+	public EntityQuery<TEntity> Sort(Expression<Func<TEntity, object>> sortExpression, bool descendingSort = false)
+	{
+		Sorts.Add(new SortField<TEntity>(sortExpression, descendingSort));
+		return this;
+	}
+
+}
---
+++ PokemonStorage.API/Data/SortField.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace PokemonStorage.API.Data;
+
+public class SortField<TEntity> where TEntity : class
+{
+    public Expression<Func<TEntity, object>> SortExpression { get; private set; }
+	public bool DescendingSort { get; private set; }
+
+	public SortField(Expression<Func<TEntity, object>> sortExpression, bool descendingSort)
+	{
+		SortExpression = sortExpression;
+		DescendingSort = descendingSort;
+	}
+}
